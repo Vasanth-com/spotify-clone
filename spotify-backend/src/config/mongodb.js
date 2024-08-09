3a64ffa38719a7f12e1e1bb0d4d8d427cd3be3bb
vasanth25 <vasanthakumar2519@gmail.com>
2024-08-09 10:21:32 +0530

    #newChanges

--- spotify-backend/src/config/mongodb.js
+++ spotify-backend/src/config/mongodb.js
@@ -5,7 +5,7 @@ const connectDB = async() =>{
         console.log("connection established");
         
     })
-    await mongoose.connect(`${process.env.MONGODB_URI}/spotify`)
+    await mongoose.connect(`mongodb+srv://vasanth:vasanth2519@cluster0.zkxkka3.mongodb.net/spotify`)
 }
 
 export default connectDB
